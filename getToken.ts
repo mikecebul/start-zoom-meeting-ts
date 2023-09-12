ab8e118af2f00e3e3ab08ef55c5f396dce1c4971
mikecebul <mikecebul@gmail.com>
2023-09-12 01:25:46 -0400

    Add s2s access token fetch

--- getToken.ts
+++ getToken.ts
@@ -1,26 +1,42 @@
-const authCode = Bun.env.AUTH_CODE;
-const clientId = Bun.env.CLIENT_ID;
-const clientSecret = Bun.env.CLIENT_SECRET;
-const redirectUri = Bun.env.REDIRECT_URI;
-
-const tokenReqBody = new URLSearchParams();
-tokenReqBody.append("grant_type", "authorization_code");
-tokenReqBody.append("code", authCode!);
-tokenReqBody.append("redirect_uri", redirectUri!);
-
-const tokenReqHeaders = {
-  Authorization:
-    "Basic " + Buffer.from(`${clientId}:${clientSecret}`).toString("base64"),
-};
-
-export async function fetchToken() {
-  const tokenResponse = await fetch("https://zoom.us/oauth/token", {
-    method: "POST",
-    headers: tokenReqHeaders,
-    body: tokenReqBody,
-  });
-
-  const tokenData = await tokenResponse.json();
-  console.log(tokenData);
-  return tokenData.access_token
+interface ZoomTokenResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
 }
+
+export async function getZoomToken(): Promise<ZoomTokenResponse | undefined> {
+  const accountId = Bun.env.S2S_ACCOUNT_ID;
+  const clientId = Bun.env.S2S_CLIENT_ID;
+  const clientSecret = Bun.env.S2S_CLIENT_SECRET;
+
+  // Encode clientId and clientSecret to base64
+  const base64Credentials = Buffer.from(`${clientId}:${clientSecret}`).toString(
+    "base64"
+  );
+
+  const url = `https://zoom.us/oauth/token?grant_type=account_credentials&account_id=${accountId}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Host: "zoom.us",
+        Authorization: `Basic ${base64Credentials}`,
+      },
+    });
+
+    if (response.ok) {
+      const json: ZoomTokenResponse = await response.json();
+      return json;
+    } else {
+      console.log(`Failed to fetch, status: ${response.status}`);
+      return undefined;
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return undefined;
+  }
+}
+
+getZoomToken();
--- index.ts
+++ index.ts
@@ -1,29 +1,30 @@
 import open from "open";
-import { fetchToken } from "./getToken";
+import { getZoomToken } from "./getToken";
 
-const accessToken = await fetchToken();
+const token = await getZoomToken();
 
-async function startZoomMeeting() {
-  const meetingId = Bun.env.MEETING_ID;
-  const bearerToken = accessToken;
+  async function startZoomMeeting() {
+    const meetingId = Bun.env.MEETING_ID;
+    const bearerToken = token?.access_token;
+  
+    const url = `https://api.zoom.us/v2/meetings/${meetingId}`;
+  
+    const headers = {
+      Accept: "application/json",
+      Authorization: `Bearer ${bearerToken}`,
+    };
+  
+    const response = await fetch(url, { headers });
+    const respJson = await response.json();
+  
+    // console.log("Response Json:", respJson);
+  
+    const startUrl = respJson["start_url"];
+  
+    console.log("Start Url:", startUrl);
+  
+    open(startUrl);
+  }
 
-  const url = `https://api.zoom.us/v2/meetings/${meetingId}`;
-
-  const headers = {
-    Accept: "application/json",
-    Authorization: `Bearer ${bearerToken}`,
-  };
-
-  const response = await fetch(url, { headers });
-  const respJson = await response.json();
-
-  console.log("Response Json:", respJson);
-
-  const startUrl = respJson["start_url"];
-
-  console.log("Start Url:", startUrl);
-
-  open(startUrl);
-}
 
 startZoomMeeting();
